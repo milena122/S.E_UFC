7d8d2941da62ce4827ec96984b7da2a202cfb0ad
André Luiz <Andr3Lu1z@users.noreply.github.com>
2019-11-12 23:54:51 -0300

    init commit

---
+++ rules.pl
@@ -0,0 +1,26 @@
+%pensamentos 
+mulher(ana, fem, 64).
+homem(joao, masc, 72).
+homem(jorge, masc, 150).
+%calculos
+flyweight(X, Y) :-
+  X =< 125, Y =<125.
+bantamweight(X, Y) :-
+  X > 125, X =< 135, Y > 125, Y =< 135.
+featherweight(X, Y):- 
+  X > 135, Y > 135, X =< 145, Y =< 145.
+lightweight(X, Y) :-
+  X > 145, Y > 145, X =< 155, Y =< 155.
+welterweight(X, Y) :-
+  X > 155, Y > 155, X =< 170, Y =< 170.
+middleweight(X, Y) :-
+  X > 170, Y > 170, X =< 185, Y <= 185.
+light_Heavyweight(X, Y) :-
+  X > 185, Y > 185, X =< 205, Y =< 205.
+heavyweight(X, Y) :-
+  X > 205, Y > 205, X =< 265, Y =< 265.
+super_Heavyweight(X, Y) :-
+  X > 265, Y > 265.
+teste_primario(X, Y, Z) :- 
+  homem(X, masc, N), homem(Y, masc, N).
+%entradas
